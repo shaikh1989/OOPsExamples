960b9b055bc7375f275cdb9b5fab55b17c234ca2
shaikh <mshaikh.it@gmail.com>
2018-02-23 10:28:17 +0530

    my pro

---
+++ src/sample/demo.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class demo {
+	public static void add()
+	{
+		System.out.println("hai hello");
+		
+	}
+	public static void main (String args[])
+	{
+		demo.add();
+		
+	}
+	static {
+		System.out.println("stataic block");
+	}
+	
+
+}
---
+++ src/sample/loop.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class loop {
+	public static void main(String args[])
+	{
+	int a=1;
+	while(a<=100)
+	{
+		System.out.println(a);
+		a++;
+		
+		
+	}
+	}
+			
+
+}
